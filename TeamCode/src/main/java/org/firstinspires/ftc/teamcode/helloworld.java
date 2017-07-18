d2d6af1961c40113eef2945d31a3e117f454d226
studentrollarobotics <studentrollarobotics@gmail.com>
2017-07-18 13:24:25 -0500

    some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloworld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helloworld.java
@@ -1,8 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 /**
  * Created by lsatt on 7/18/2017.
  */
 
-public class helloworld {
+public class helloworld extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }

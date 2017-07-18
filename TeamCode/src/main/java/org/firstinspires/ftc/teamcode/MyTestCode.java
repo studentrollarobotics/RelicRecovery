184674729da8f147c4df366e65050491b627b3d6
studentrollarobotics <studentrollarobotics@gmail.com>
2017-07-18 14:25:51 -0500

    finished the program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTestCode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -13,9 +14,10 @@ public class MyTestCode extends OpMode{
 
     DcMotor RightMotor;
     DcMotor LeftMotor;
-
+    DcMotor LiftMotor;
     double RightPower;
     double LeftPower;
+    double LiftPower;
 
 
 
@@ -23,11 +25,28 @@ public class MyTestCode extends OpMode{
     public void init() {
         RightMotor = hardwareMap.dcMotor.get("rm");
         LeftMotor = hardwareMap.dcMotor.get("lm");
-        
+        LiftMotor = hardwareMap.dcMotor.get("lift");
+        RightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        LiftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
+    RightPower = gamepad1.right_stick_y;
+    LeftPower = gamepad1.left_stick_y;
+
+    RightMotor.setPower(RightPower);
+    LeftMotor.setPower(LeftPower);
+
+
+    if (gamepad1.right_bumper){
+            LiftPower = 1;
+        }
+    else if (gamepad1.left_bumper){
+          LiftPower = -1;
+    }
+    else
+          LiftPower = 0;
 
     }
 }
